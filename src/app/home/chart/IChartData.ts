d71467885a74a66112172aa2829e202c924d8864
Lucas Nascimento <lucasnascimentocontato@gmail.com>
2021-09-08 14:49:17 -0300

    Refactor: include path to backend in http-service

--- src/app/home/chart/IChartData.ts
+++
@@ -1,4 +0,0 @@
-export interface IChartData {
-  name: string;
-  y: number;
-}
--- src/app/login/iuser.ts
+++
@@ -1,5 +0,0 @@
-import { BaseModel } from '../shared/crude.service';
-export interface User extends BaseModel {
-  usuario: string;
-  senha: string;
-}
